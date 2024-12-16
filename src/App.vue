ba747d249a77e961439f9ff6edb47698a23bfda3
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-12-16 18:13:25 +0400

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="app">
+		<h1 class="title">TEMPLATE_VUE_3_VITE</h1>
+		<h2 class="title">TEMPLATE_VUE_3_VITE</h2>
+		<h3 class="title">TEMPLATE_VUE_3_VITE</h3>
+		<h4 class="title">TEMPLATE_VUE_3_VITE</h4>
+		<h5 class="title">TEMPLATE_VUE_3_VITE</h5>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
